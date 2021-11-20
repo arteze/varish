eebca7346a080e6ef4a52327c164d1afb793b293
ArtEze <passymas@gmail.com>
2021-11-20 17:08:15 -0300

    Create lnul6.js

---
+++ js/lnul6.js
@@ -0,0 +1,5 @@
+#/bin/node
+
+var programa = process.argv.slice(2).join(" ")
+var a = child_process.execSync(`r && strace ${programa} 2>&1 | grep -i such`).toString().split("\n")[1].split("\"")
+console.log(a[2].includes("ENOENT")&&a[1]||"")
