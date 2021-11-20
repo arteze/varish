8b38290d870320007a1428140069f117d05a0414
ArtEze <passymas@gmail.com>
2021-11-20 17:13:41 -0300

    Update lnul6.js

--- js/lnul6.js
+++ js/lnul6.js
@@ -1,5 +1,6 @@
 #/bin/node
 
+var child_process = require("child_process")
 var programa = process.argv.slice(2).join(" ")
 var a = child_process.execSync(`r && strace ${programa} 2>&1 | grep -i such`).toString().split("\n")[1].split("\"")
-console.log(a[2].includes("ENOENT")&&a[1]||"")
+console.log(a[2] && a[2].includes("ENOENT")&&a[1]||"")
