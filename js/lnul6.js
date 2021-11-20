b1d4997136f820f97779cb6f1dc2cee4b9965bc6
ArtEze <passymas@gmail.com>
2021-11-20 18:35:45 -0300

    Update lnul6.js

--- js/lnul6.js
+++ js/lnul6.js
@@ -2,5 +2,6 @@
 
 var child_process = require("child_process")
 var programa = process.argv.slice(2).join(" ")
-var a = child_process.execSync(`tput reset && strace ${programa} 2>&1 | grep -i such`).toString().split("\n")[1].split("\"")
-console.log(a[2] && a[2].includes("ENOENT")&&a[1]||"")
+var clave = "ENOENT"
+var a = child_process.execSync(`tput reset && strace ${programa} 2>&1 | grep -i ${clave}`).toString().split("\n")[1].split("\"")
+console.log(a[2] && a[2].includes(clave)&&a[1]||"")
