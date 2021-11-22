7338f1001529929b60cf06f71174227e2f4cd82a
ArtEze <passymas@gmail.com>
2021-11-22 09:44:13 -0300

    Update instalar_txz.sh

--- instalar_txz.sh
+++ instalar_txz.sh
@@ -1,8 +1,18 @@
 #/bin/sh
 
-ruta=/tmp/paquete
-mkdir -p $ruta
-tar -C $ruta -v -xf $1
-cp -vrf $ruta/* /
-rm -vrf $ruta/*
-[[ "$2" == ""  ]] && gxmessage -print "Se instaló $1" -center -borderless -sticky -timeout 2 -noescape -nofocus -ontop -buttons ""
+if [[ "$1" != "" ]];then
+  ruta=/tmp/paquete
+  nombre="$(basename "$1")"
+  mkdir -p $ruta
+  tar -C $ruta -v -xf $1
+  mkdir "$ruta/$nombre"
+  mv "$ruta/install/"* "$ruta/$nombre"
+  mv "$ruta/$nombre" "$ruta/install"
+  cp -vrf $ruta/* /
+  rm -vrf $ruta/*
+  [[ "$2" == ""  ]] && (
+    gxmessage -print "Se instaló $1" -center -borderless -sticky -timeout 2 -noescape -nofocus -ontop -buttons ""
+  )
+else
+  echo "No hay archivo"
+fi
